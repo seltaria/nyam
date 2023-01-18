51bb7fca7dd68cffca2c0ad7d2952aa93440abea
Бондарева Анастасия <seltaria@mail.ru>
2023-01-18 18:34:58 +0300

    add data file

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    taste: "с фуа-гра",
+    description: ["10 порций", "мышь в подарок"],
+    weight: 0.5,
+    inStock: true,
+    footerText: "Печень утки разварная с артишоками.",
+  },
+  {
+    id: 2,
+    taste: "с рыбой",
+    description: ["40 порций", "2 мыши в подарок"],
+    weight: 2,
+    inStock: true,
+    footerText: "Головы щучьи с чесноком да свежайшая сёмгушка.",
+  },
+  {
+    id: 3,
+    taste: "с курой",
+    description: ["100 порций", "5 мышей в подарок", "заказчик доволен"],
+    weight: 5,
+    inStock: false,
+    footerText: "Филе из цыплят с трюфелями в бульоне.",
+  },
+]
